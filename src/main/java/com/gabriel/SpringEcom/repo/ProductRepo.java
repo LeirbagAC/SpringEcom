97d6a354d03ae8753fb0086c0514e74a5b10fe53
LeirbagAC <albuquerqueg373@gmail.com>
2026-08-03 14:53:07 -0300

    refactor(cart): busca somente produtos ativos

--- src/main/java/com/gabriel/SpringEcom/repo/ProductRepo.java
+++ src/main/java/com/gabriel/SpringEcom/repo/ProductRepo.java
@@ -1,20 +1,22 @@
 package com.gabriel.SpringEcom.repo;
 
 import com.gabriel.SpringEcom.model.Product;
+import com.gabriel.SpringEcom.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE " +
+    @Query("SELECT p FROM Product p WHERE p.active = true AND (" +
             "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
             "LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')))")
     List<Product> searchProducts(String keyword);
 
 }
